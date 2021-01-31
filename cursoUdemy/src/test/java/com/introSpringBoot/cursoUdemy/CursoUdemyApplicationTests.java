3fb73958e11950d8818bdc930410f7ff1d41d54f
Evelyn Alves <prof_aulasevelyn@hotmail.com>
2021-01-31 14:33:48 -0300

    Project Created

---
+++ cursoUdemy/src/test/java/com/introSpringBoot/cursoUdemy/CursoUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.introSpringBoot.cursoUdemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
